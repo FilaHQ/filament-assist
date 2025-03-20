e8093088738ab5e3dd0ad0b2ccf27b23ee25de3d
Knight Tan <knight@reworklabs.com>
2025-03-21 06:53:33 +0800

    added form and table action

---
+++ src/Tables/Actions/AssistAction.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace FilaHQ\FilamentAssist\Tables\Actions;
+
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components;
+use Filament\Support\Enums\MaxWidth;
+use FilaHQ\FilamentAssist\Models\Assist;
+use Filament\Notifications\Notification;
+use Closure;
+
+class AssistAction extends Action
+{
+    protected string $type = 'assist';
+    protected string|Closure|null $source = null;
+
+    public static function getDefaultName(): ?string
+    {
+        return 'assist';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->form($this->modalForm());
+        $this->modalWidth(MaxWidth::Large);
+        $this->action($this->assist(...));
+    }
+
+    protected function modalForm(): array
+    {
+        return [
+            Components\TextInput::make('title')->required(),
+            Components\Textarea::make('content')->required(),
+        ];
+    }
+
+    public function defaultAssistType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function source(string|Closure|null $source = null): static
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    public function getSource(): ?string
+    {
+        return $this->evaluate($this->source);
+    }
+
+    protected function assist(array $data, $livewire): void
+    {
+        $type = filament()->hasPlugin('filament-assist')
+            ? filament('filament-assist')->type()
+            : $this->type;
+        $data['user_id'] = auth()->user()->id;
+        $data['type'] = $type;
+        $data['source'] = $this->getSource() ?? $livewire->getName();
+        Assist::create($data);
+        Notification::make()
+            ->title(ucfirst($this->label ?? $this->name) . ' sent successfully')
+            ->success()
+            ->send();
+    }
+}
